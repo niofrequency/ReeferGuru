7139ea1f9682cd3e442030a0290afac394edd6f6
niofrequency <124836368+niofrequency@users.noreply.github.com>
2025-12-15 12:12:01 +0900

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,40 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+export type ClassValue = string | number | boolean | undefined | null | { [key: string]: any } | ClassValue[];
+
+function toVal(mix: any): string {
+  let k, y, str = '';
+
+  if (typeof mix === 'string' || typeof mix === 'number') {
+    str += mix;
+  } else if (typeof mix === 'object') {
+    if (Array.isArray(mix)) {
+      for (k = 0; k < mix.length; k++) {
+        if (mix[k] && (y = toVal(mix[k]))) {
+          str && (str += ' ');
+          str += y;
+        }
+      }
+    } else {
+      for (k in mix) {
+        if (mix[k]) {
+          str && (str += ' ');
+          str += k;
+        }
+      }
+    }
+  }
+
+  return str;
+}
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  let i = 0, tmp, x, str = '';
+  while (i < inputs.length) {
+    if (tmp = inputs[i++]) {
+      if (x = toVal(tmp)) {
+        str && (str += ' ');
+        str += x;
+      }
+    }
+  }
+  return str;
 }
