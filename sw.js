c97062e2c68da0ea491d8795bf071999235cf8a9
niofrequency <124836368+niofrequency@users.noreply.github.com>
2026-05-20 09:04:45 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'reefer-guru-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Add other critical assets if needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
